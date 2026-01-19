d98949ecc0bd9de289b74b9cdd8fdcf6c119d148
RajGehlot28 <rajgehlot2007@gmail.com>
2026-01-19 14:07:01 +0530

    Add files via upload

---
+++ ParkingSystem/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome To GIT:");
+        ParkingSystem parking = new ParkingSystem();
+        parking.parkingSystem();
+    }
+}
