cb66e198f00e941714f6ef61656015a17ac8743f
RajGehlot28 <rajgehlot2007@gmail.com>
2026-01-19 14:05:52 +0530

    Delete ParkingSystem directory

--- ParkingSystem/Input.java
+++
@@ -1,6 +0,0 @@
-import java.util.*;
-public class Input {
-   public Scanner userInput() {
-      return new Scanner(System.in);
-   } 
-}
--- ParkingSystem/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome To GIT:");
-        ParkingSystem parking = new ParkingSystem();
-        parking.parkingSystem();
-    }
-}
