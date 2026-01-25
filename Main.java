34611efece4e712f9d7fbffb8fd835ccc3ecc013
Varshika Sharma <sharmavarshika3@gmail.com>
2026-01-25 15:19:38 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        College git = new College(1, 20);
+//
+//        git.addStudent(new Student("Varshika", 104));
+//        git.addStudent(new Student("Anjali", 101));
+//        git.addStudent(new Student("Vivek", 102));
+//
+//        System.out.println(git);
+//    }
+//}
